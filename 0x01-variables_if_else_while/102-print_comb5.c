70bba7ca4a17ee95f1b673cadbce1e81da651706
dealyssa <basseyalice@outlook.com>
2022-03-14 06:21:36 -0700

    combination of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+/*
+ * File: 102-print_comb5.c
+ * Auth: Precious Bassey
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible  combination of two two-digit numbers,
+ *	ranging from 0-99, seperated by a comma followed bya space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 =0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
