7eb67d342e65b30499ff1401bd4b03f6a7016758
dealyssa <basseyalice@outlook.com>
2022-03-16 07:22:47 -0700

    to lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,15 +10,12 @@
  */
 int main(void)
 {
-	print_alphabet();
-	{
-		char letter;
+	char letter;
 		
-		for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	}
 
 	putchar('\n');
 
-	return (0);
+	print_alphabet();
 }
