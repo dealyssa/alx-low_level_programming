accf20da4895b4dd1cc8f4077592097011288d63
dealyssa <basseyalice@outlook.com>
2022-04-01 07:42:32 -0700

    static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,3 @@
-#ifndef main.h
-#define main.h
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -16,10 +13,8 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
-char *memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *strpbrk(char *s, char *accept );
-char *_strstr(char *haystavk, char *needle);
-
-#endif
+char *_strpbrk(char *s, char *accept );
+char *_strstr(char *haystack, char *needle);
